0516bd4597ca87fcdcff7c1259bd5937c0bae245
unknown <filip.trifunovic23@gmail.com>
2023-03-14 13:54:37 +0100

    finish season/episodes page

--- src/pages/season.tsx
+++ src/pages/season.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "../components/image";
 import Section from "../components/section";
 import { Film } from "../interfaces";
@@ -29,6 +29,21 @@ const Season = () => {
     title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
   });
 
+  const [episodes, setEpisodes] = useState<any[]>();
+
+  const fetch = () => {
+    const arrs: any[] = [];
+
+    for (let i = 0; i < 12; i++) {
+      arrs.push({});
+    }
+    setEpisodes(arrs);
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <>
       <div className="h-[150px] top-0 left-0 right-0 relative">
@@ -42,8 +57,35 @@ const Season = () => {
         ></Image>
         <div className="px-3 flex flex-col gap-3">
           <p className="text-xl line-clamp-1">{film.title}</p>
-          <p className="line-clamp-3 opacity-[0.9]">Season 1</p>
+          <p className="opacity-[0.9]">
+            Season 1 | {episodes?.length} episodes{" "}
+          </p>
         </div>
+      </Section>
+      <Section title="Episodes">
+        {episodes?.map((episode, i) => (
+          <div
+            className="my-6 flex items-stretch gap-4 rounded-md overflow-hidden cursor-pointer hover:bg-primary px-3 py-1.5"
+            key={i}
+          >
+            <Image src="" className="w-[300px] min-w-[300px] h-[350px]"></Image>
+            <div className="overflow-hidden flex flex-col gap-3">
+              <p className="text-lg truncate">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Voluptate nemo consequatur culpa molestias, natus consectetur,
+                dignissimos, veniam eveniet impedit veritatis iure adipisci
+                nihil? Recusandae corrupti atque, aliquam quod esse optio!
+              </p>
+              <p className="opacity-[0.9] line-clamp-5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+                deleniti nemo ex sunt dolor. Minus ipsa asperiores hic
+                voluptatem laborum exercitationem animi, eos corrupti recusandae
+                molestias quasi voluptate ad facilis.
+              </p>
+              <div className="mt-auto pt-3 text-right">14 March 2023</div>
+            </div>
+          </div>
+        ))}
       </Section>
     </>
   );
