c46d3663ab5c9681e8b7c4457ce5e259410bfa9a
unknown <filip.trifunovic23@gmail.com>
2023-03-14 15:41:33 +0100

    func for getting trendings

--- src/api/tmdb-api.ts
+++ src/api/tmdb-api.ts
@@ -1,4 +1,7 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { Film } from "../interfaces";
+import { MediaType } from "../types";
+import { formatResult } from "../utils";
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_TMDB_API_URL,
@@ -12,3 +15,20 @@ axiosClient.interceptors.request.use((config) => {
     },
   };
 });
+
+export const getTrendings = async (mediaType: MediaType): Promise<Film[]> => {
+  try {
+    const { data } = await axiosClient.get<
+      any,
+      AxiosResponse<{
+        results: unknown[];
+      }>
+    >(`/trending/${mediaType}/week`);
+
+    return data.results.map((val) => formatResult(mediaType, val));
+  } catch (error) {
+    console.error(error);
+  }
+
+  return [];
+};
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,19 @@
+import { Film } from "./interfaces";
+import { MediaType } from "./types";
+
 export const mergeClassName = (val1: string, val2?: string) => {
   return val1 + " " + (val2 || "");
+};
+
+export const formatResult = (mediaType: MediaType, obj: any): Film => {
+  return {
+    id: obj.id,
+    title: obj.title || obj.name,
+    description: obj.overview,
+    coverPath: obj.backdrop_path,
+    posterPath: obj.poster_path,
+    genreIds: obj.genre_ids || [],
+    mediaType,
+    seasons: obj.seasons || [],
+  };
 };
