08d62907d9bc840ca92846f6bc851f059a980c8a
unknown <filip.trifunovic23@gmail.com>
2023-03-14 14:24:50 +0100

    footer

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div className="p-6 text-center">
+      <p className="text-sm opacity-[0.7]">
+        &#169; 2023 MovieSpace. All rigts reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
